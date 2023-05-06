235b7c4f2248218f924e5d50853c4d89a4eb9115
Guywash-Ka <rodvlasov2003@yandex.ru>
2023-05-06 19:00:22 +0300

    Add permisson check before launch

--- app/src/main/java/com/internship/filemanager/MainActivity.kt
+++ app/src/main/java/com/internship/filemanager/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.internship.filemanager
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.internship.filemanager.ui.screens.MainScreen
 import com.internship.filemanager.ui.theme.FileManagerTheme
 
+
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FileManagerTheme {
-                    MainScreen()
+                FileManagerApp()
             }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.R)
+@Composable
+fun FileManagerApp() {
+    val isPermitted = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        Environment.isExternalStorageManager()
+    } else
+        ContextCompat.checkSelfPermission(LocalContext.current, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    if (isPermitted) {
+        MainScreen()
+    } else {
+        LocalContext.current.startActivity(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
     }
 }
